fae88f74233b5aaca4f85b1c49516701344d0576
Evgeny Makarov <makar-81@mail.ru>
2020-04-09 09:18:00 +0300

    work_2.8 (2)

--- 2 week/work_2.8.py
+++ 2 week/work_2.8.py
@@ -1,7 +1,7 @@
 n = int(input())
 m = int(input())
 k = int(input())
-if k < m * n and ((k % m == 0 and k // m <= n) or (k % n == 0 and k // n <= m)):
+if (k % m == 0 and k // m <= n) or (k % n == 0 and k // n <= m):
     print("YES")
 else:
     print("NO")
