c6093e003972208a6967ddb28a613a27f58186ff
Makar-81 <makar-81@mail.ru>
2021-09-15 14:20:36 +0300

    commit 1

--- Yandex_samples/Example/src/Example.java
+++ Yandex_samples/Example/src/Example.java
@@ -4,6 +4,12 @@ public class Example {
     public static void main(String[] args) throws Exception {
         BufferedReader string = new BufferedReader(new InputStreamReader(System.in));
         String start_line = string.readLine();
-        System.out.println(start_line);
+        int space = start_line.indexOf(" ");
+        int A = Integer.parseInt(start_line.substring(0, space));
+        //System.out.println(start_line.length());
+        //System.out.println(start_line.substring(1));
+        //System.out.println(start_line.substring(space+1));
+        int B = Integer.parseInt(start_line.substring(space+1));
+        System.out.println(A + B);
     }
 }
---
+++ Yandex_samples/Example2/Example2.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Example2 {
+        public static void main(String[] args) {
+            try {
+                BufferedReader br;
+                BufferedWriter bw;
+    
+                FileReader fr;
+                //BufferedInputStream fr;
+                //OutputStream bw;
+                FileWriter wr;
+                
+                fr = new FileReader("input.txt");
+                wr = new FileWriter("output.txt");
+                
+                br = new BufferedReader(fr);
+                bw = new BufferedWriter(wr);
+                
+                //br = new BufferedReader(new InputStreamReader(System.in));
+                //bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+                String start_line = br.readLine();
+                int A = Integer.parseInt(start_line.substring(0, start_line.indexOf(" ")));
+                int B = Integer.parseInt(start_line.substring(start_line.indexOf(" ")+1));
+    
+                System.out.println(A+B);
+                bw.write(A+B);
+                bw.flush();
+                bw.close();
+                br.close();
+
+            } catch (FileNotFoundException e) {
+                
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+      
+    }    
+}
