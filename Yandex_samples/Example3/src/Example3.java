b8c3d1bbc2732fc5734a2142ba5a4b26a1c520db
Makar-81 <makar-81@mail.ru>
2021-09-15 15:00:06 +0300

    commit new version

---
+++ Yandex_samples/Example3/src/Example3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Example3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader string = new BufferedReader(new InputStreamReader(System.in));
+        String J = string.readLine();
+        String S = string.readLine();
+        int sum = 0;
+
+        for (int i =0; i < J.length();i++) {
+              sum =   
+        }
+        
+        System.out.println(A + B);
+    }
+}
